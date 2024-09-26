0b1aa1564d1ae97e31a8945fd1b77e4285185220
Vinay Malyala <srivinay1432@gmail.com>
2024-09-26 19:37:30 +0530

    added header

---
+++ script.js
@@ -0,0 +1,28 @@
+// show menu
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+
+// Menu show 
+if(navToggle){
+    navToggle.addEventListener("click", function(){
+        navMenu.classList.add("show-menu");
+    });
+}
+
+// Menu hidden 
+if(navClose){
+    navClose.addEventListener("click", function(){
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+// remove menu mobile 
+const navLink = document.querySelectorAll(".nav_link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+}
+
+navMenu.forEach(n => n.addEventListener('click', linkAction));
