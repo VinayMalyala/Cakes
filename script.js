d27d330ef243f599ad1642b9539984111598d9ba
Vinay Malyala <srivinay1432@gmail.com>
2024-09-29 18:40:12 +0530

    added scroll-up

--- script.js
+++ script.js
@@ -45,3 +45,10 @@ window.addEventListener('scroll', shadowHeader);
 //         centeredSlides: "auto",
 //     });
 // });
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll") : scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener('scroll', scrollUp);
