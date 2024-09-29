022627b2bdf46d813382e01a4e303ec98f31cbc5
Vinay Malyala <srivinay1432@gmail.com>
2024-09-29 18:56:19 +0530

    added active link

--- script.js
+++ script.js
@@ -46,9 +46,31 @@ window.addEventListener('scroll', shadowHeader);
 //     });
 // });
 
+// show scroll up 
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up');
     this.scrollY >= 350 ? scrollUp.classList.add("show-scroll") : scrollUp.classList.remove("show-scroll");
 }
 
 window.addEventListener('scroll', scrollUp);
+
+// scroll section active links 
+const sections = document.querySelectorAll("section[id");
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']');
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link');
+        }else{
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
